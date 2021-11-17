0bad953d93a916ecc8286931667b3e683a91b1f8
Caleb Kim <kim.caleb10@gmail.com>
2021-11-17 14:48:56 -0800

    Optimized rotateArray

--- rotateArray.js
+++ rotateArray.js
@@ -1,9 +1,8 @@
 var rotate = function(nums, k) {
   var index = 0;
   k > nums.length ? index = k % nums.length : index = k;
-  let chunk = nums.splice(index - 1);
-  nums = chunk.concat(nums);
-  return nums;
+  let chunk = nums.splice(nums.length - index);
+  nums.splice(0, 0, ...chunk);
 };
 
 console.log(rotate([1, 2, 3, 4, 5, 6, 7], 3));
