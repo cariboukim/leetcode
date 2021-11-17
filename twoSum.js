099bde2f95fa6d9e13a46f3f2b5d569c5b6d4176
Caleb Kim <kim.caleb10@gmail.com>
2021-11-17 14:41:39 -0800

    Optimization of TwoSum

--- twoSum.js
+++ twoSum.js
@@ -1,17 +1,27 @@
+// var twoSum = function(nums, target) {
+//   var sol;
+//   // iterate over the nums array
+//   for (let i = 0; i < nums.length; i++) {
+//     // iterate over nums array with j, which is i + 1
+//     for (let j = i + 1; j < nums.length; j++) {
+//       // if i + j equals target
+//       if (target === nums[i] + nums[j]) {
+//         // return i and j in tuple form
+//         sol = [i, j];
+//       }
+//     }
+//   }
+//   return sol;
+// };
+
 var twoSum = function(nums, target) {
-  var sol;
-  // iterate over the nums array
+  let hash = {};
+
   for (let i = 0; i < nums.length; i++) {
-    // iterate over nums array with j, which is i + 1
-    for (let j = i + 1; j < nums.length; j++) {
-      // if i + j equals target
-      if (target === nums[i] + nums[j]) {
-        // return i and j in tuple form
-        sol = [i, j];
-      }
-    }
+    if (nums[i] in hash) return [hash[nums[i]], i];
+
+    hash[target - nums[i]] = i;
   }
-  return sol;
 };
 
 console.log(twoSum([1, 2, 3, 4], 7));
