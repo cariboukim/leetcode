2835a982d66959abd8220fe6f7823355c77c085b
Caleb Kim <kim.caleb10@gmail.com>
2021-11-17 14:53:17 -0800

    Optimized searchArrayInsert

--- searchInsertPosition.js
+++ searchInsertPosition.js
@@ -1,25 +1,39 @@
+// var searchInsert = function(nums, target) {
+//   let n = 0;
+//   let i = 0;
+//   while (i < nums.length) {
+//     if (nums[i] === target) {
+//       return n = i;
+//     } else if (nums[i] > target) {
+//       return n = i;
+//     } else {
+//       n = -1;
+//     }
+
+//     i++;
+//   }
+
+//   if (n === -1) {
+//     if (target > nums[nums.length - 1]) {
+//       return n = nums.length;
+//     } else if (target < nums[0]) {
+//       return n = 0;
+//     }
+//   }
+// };
+
 var searchInsert = function(nums, target) {
-  let n = 0;
-  let i = 0;
-  while (i < nums.length) {
-    if (nums[i] === target) {
-      return n = i;
-    } else if (nums[i] > target) {
-      return n = i;
-    } else {
-      n = -1;
-    }
+  const arrLength = nums.length;
+  let a = 0;
 
-    i++;
-  }
-
-  if (n === -1) {
-    if (target > nums[nums.length - 1]) {
-      return n = nums.length;
-    } else if (target < nums[0]) {
-      return n = 0;
+  for (let i = 0; i < arrLength; i++) {
+    if (i === arrLength - 1 && nums[i] !== target) a = i + 1;
+    if (nums[i] == target || nums[i] > target) {
+      return a = i;
     }
   }
-};
 
-console.log(searchInsert([1, 3, 5, 6], 0));
+  return a;
+}
+
+console.log(searchInsert([1, 3, 5, 6], 3));
