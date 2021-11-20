688d8580f44aaabba2a42d8879c329053fe7e6ae
Caleb Kim <kim.caleb10@gmail.com>
2021-11-20 15:40:10 -0800

    Optimized bestTimeToSellAndBuyStock

--- bestTimeToSellAndBuyStock.js
+++ bestTimeToSellAndBuyStock.js
@@ -1,18 +1,39 @@
+// unoptimized
+
+// const maxProfit = (prices) => {
+
+//   let low = prices[0];
+
+//   let profit = 0;
+
+//   for(let i = 0; i < prices.length; i++) {
+//       if ( prices[i] < low ) {
+//           low = prices[i];
+//       }
+//       if ( prices[i] - low > profit) {
+//           profit = prices[i] - low;
+//       }
+//   }
+
+//   return profit;
+
+// };
+
+// optimized
+
 const maxProfit = (prices) => {
+  let max = 0;
+  let min = prices[0];
 
-  let low = prices[0];
-
-  let profit = 0;
-
-  for(let i = 0; i < prices.length; i++) {
-      if ( prices[i] < low ) {
-          low = prices[i];
-      }
-      if ( prices[i] - low > profit) {
-          profit = prices[i] - low;
-      }
+  for (let i = 0; i < prices.length; i++) {
+    let current = prices[i];
+    min = Math.min(min, current);
+    max = Math.max(max, current - min);
   }
 
-  return profit;
-
+  return max;
 };
+
+let stocks = [7,1,5,3,6,4];
+
+console.log(maxProfit(stocks));
