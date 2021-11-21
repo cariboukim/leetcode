15179be0f92fcc6a8fb4a18060ec189d012b22be
Caleb Kim <kim.caleb10@gmail.com>
2021-11-20 16:11:54 -0800

    Optimized space and time complexity for singleElementInASortedArray

--- singleElementInASortedArray.js
+++ singleElementInASortedArray.js
@@ -1,14 +1,26 @@
 let nums = [1, 1, 2, 3, 3, 4, 4, 8, 8];
 
+// unoptimized / O(n) space complexity
+
+// var singleNonDuplicate = (nums) => {
+//   let count = [] ;
+//   let single = 0;
+
+//   for (let i = 0; i < nums.length; i++) {
+//     count.indexOf(nums[i]) > -1 ? count.pop() : count.push(nums[i]);
+//   }
+
+//   return count[0];
+// }
+
 var singleNonDuplicate = (nums) => {
-  let count = [] ;
-  let single = 0;
-
-  for (let i = 0; i < nums.length; i++) {
-    count.indexOf(nums[i]) > -1 ? count.pop() : count.push(nums[i]);
+  for (let i = 0; i < nums.length; i += 2) {
+    if (nums[i] === nums[i + 1]) {
+      continue;
+    } else {
+      return nums[i];
+    }
   }
-
-  return count[0];
 }
 
-console.log(nonSingleDuplicate(nums));
+console.log(singleNonDuplicate(nums));
