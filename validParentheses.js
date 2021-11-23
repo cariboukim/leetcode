c53e3b96cb13d9c0a30b6f77e78e4c1ec367e2a9
Caleb Kim <kim.caleb10@gmail.com>
2021-11-23 15:54:49 -0800

    Used recursion for validParentheses

--- validParentheses.js
+++ validParentheses.js
@@ -6,27 +6,58 @@
 // 2. Open brackets must be closed in the correct order.
 
 let parens = '({[]})';
-let parensFalse = '[{}';
+let parensFalse = '[}';
 let parensSingle = '(';
+
+// var isValid = (s) => {
+//   if (s.length === 1) return false;
+//   let count = {};
+
+//   for (let i = 0; i < s.length; i++) {
+//     !count[s[i]] ? count[s[i]] = 1 : count[s[i]] ++;
+//   }
+
+//   if (count['{'] !== count['}'] || count['('] !== count[')'] || count['['] !== count[']']) {
+//     return false;
+//   } else {
+//     return true;
+//   }
+
+// };
+
+// first attempt doesn't check for complete parentheses pairs but just not ordered. need to check for pairs and ordered now!
 
 var isValid = (s) => {
   if (s.length === 1) return false;
-  let count = {};
 
-  for (let i = 0; i < s.length; i++) {
-    !count[s[i]] ? count[s[i]] = 1 : count[s[i]] ++;
+  let test = ['[]', '{}', '()'];
+
+  let index = 0;
+
+  let array = s.split('');
+
+  const recursion = (a, i) => {
+    if (i === a.length - 1 || a.length === 0) {
+      return;
+    } else {
+      if (test.indexOf(a[i] + a[i + 1]) > -1) {
+        a.splice(i, 2);
+        return recursion(a, i -= 1);
+      } else {
+        return recursion(a, i += 1);
+      }
+    }
   }
+  recursion(array, index);
 
-  if (count['{'] !== count['}'] || count['('] !== count[')'] || count['['] !== count[']']) {
-    return false;
-  } else {
+  if (array.length === 0) {
     return true;
+  } else {
+    return false;
   }
+}
 
-};
 
-console.log(isValid(parensSingle));
-
+// console.log(isValid(parensSingle));
 console.log(isValid(parensFalse));
-
-console.log(isValid(parens));
+// console.log(isValid(parens));
