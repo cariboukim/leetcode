80c03dead9026c0609748e8c3b133d3dcc26b1e8
Caleb Kim <kim.caleb10@gmail.com>
2021-11-23 16:35:22 -0800

    Fastest submission on LeetCode

--- mergeTwoSortedLists.js
+++ mergeTwoSortedLists.js
@@ -62,6 +62,35 @@ var mergeTwoLists = function(l1, l2) {
   }
 };
 
+// fastest submission on leetcode
+
+var mergeTwoLists = function(l1, l2) {
+  let list = new ListNode()
+  let head = list
+
+  while (l1 !== null && l2 !== null) {
+
+
+      if (l1.val < l2.val) {
+          list.next = new ListNode(l1.val)
+          l1 = l1.next
+      } else {
+          list.next = new ListNode(l2.val)
+          l2 = l2.next
+      }
+
+      list = list.next
+  }
+
+  if (l1 !== null)
+      list.next = l1
+  if (l2 !== null)
+      list.next = l2
+
+
+  return head.next
+};
+
 console.log(mergeTwoLists(list1, list2));
 console.log(mergeTwoLists(list3, list4));
 console.log(mergeTwoLists(list5, list6));
