7195f06528abede34caa201b3b475de51690aeec
Caleb Kim <kim.caleb10@gmail.com>
2021-11-23 16:30:45 -0800

    Using linkedList functions and implementing recursion

--- mergeTwoSortedLists.js
+++ mergeTwoSortedLists.js
@@ -44,6 +44,24 @@ var mergeTwoLists = function(l1, l2) {
   return final;
 };
 
+// this works with arrays but we have to use linked lists, as listed in the example above, so we have to use certain functions as shown in this example
+
+// recursive example of using linkedList functions
+
+var mergeTwoLists = function(l1, l2) {
+
+  if(!l1) return l2;
+  if(!l2) return l1;
+
+  if(l1.val < l2.val){
+      l1.next = mergeTwoLists(l1.next, l2);
+      return l1;
+  }else{
+      l2.next = mergeTwoLists(l1, l2.next);
+      return l2;
+  }
+};
+
 console.log(mergeTwoLists(list1, list2));
 console.log(mergeTwoLists(list3, list4));
 console.log(mergeTwoLists(list5, list6));
