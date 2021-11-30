6788c73fd635688c347f0cc46253871e9f3a9561
Caleb Kim <kim.caleb10@gmail.com>
2021-11-29 18:48:58 -0800

    Initial Solution

---
+++ lengthOfLastWord.js
@@ -0,0 +1,19 @@
+/*
+@param {string} s
+@return {number}
+*/
+
+// Given a string s consisting of some words separated by some number of spaces, return the length of the last word in the string.
+
+let test1 = 'Hello World';
+let test2 = '   fly me   to   the moon  ';
+
+// initial solution
+
+var lengthOfLastWord = function(s) {
+  let sArray = s.split(' ');
+  return sArray.pop().length;
+};
+
+console.log(lengthOfLastWord(test1));
+console.log(lengthOfLastWord(test2));
