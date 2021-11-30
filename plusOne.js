7137b88078c4366f988f3c217f134901434a0d07
Caleb Kim <kim.caleb10@gmail.com>
2021-11-29 19:17:20 -0800

    leetcode discussion solution

--- plusOne.js
+++ plusOne.js
@@ -9,20 +9,33 @@ let test3 = [9];
 let test4 = [0];
 
 // initial solution
+// for some reason, using Number() on test2 yields the same number but the ending three digits all becoming 0. no explanation for this right now but hopefully come up with answers soon
+
+// var plusOne = (digits) => {
+//   let joined = digits.join('');
+//   joined++;
+//   joined = JSON.stringify(joined);
+//   let result = [];
+//   for (let i = 0; i < joined.length; i++) {
+//     result.push(Number(joined[i]));
+//   }
+//   return result;
+// }
+
+// leetcode discussion solution
 
 var plusOne = (digits) => {
-  let joined = digits.join('');
-  console.log(joined);
-  console.log(parseInt(joined));
-  joined = JSON.stringify(joined);
-  let result = [];
-  for (let i = 0; i < joined.length; i++) {
-    result.push(Number(joined[i]));
+  let i = digits.length - 1;
+  while (i > -1) {
+    digits[i]++;
+    if (digits[i] < 10) return digits;
+    digits[i] = 0;
+    i--;
   }
-  return result;
+  return [1, ...digits];
 }
 
-// console.log(plusOne(test1));
+console.log(plusOne(test1));
 console.log(plusOne(test2));
-// console.log(plusOne(test3));
-// console.log(plusOne(test4));
+console.log(plusOne(test3));
+console.log(plusOne(test4));
