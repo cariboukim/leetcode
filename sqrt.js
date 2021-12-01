dcac5096bcdc0c49e09229b9699da238543f44d7
Caleb Kim <kim.caleb10@gmail.com>
2021-11-30 16:30:38 -0800

    Fastest solution on leetcode

--- sqrt.js
+++ sqrt.js
@@ -8,19 +8,25 @@
 let test1 = 4;
 let test2 = 8;
 
+// var mySqrt = (x) => {
+//   let sqrt = 0;
+//   let i = 1;
+
+//   while (i < 100000) {
+//     if (i**2 > x) {
+//       sqrt = i - 1;
+//       break;
+//     }
+//     i++;
+//   }
+
+//   return sqrt;
+// }
+
+// fastest solution on leetcode
+
 var mySqrt = (x) => {
-  let sqrt = 0;
-  let i = 1;
-
-  while (i < 100000) {
-    if (i**2 > x) {
-      sqrt = i - 1;
-      break;
-    }
-    i++;
-  }
-
-  return sqrt;
+  return parseInt(Math.sqrt(x));
 }
 
 console.log(mySqrt(test1));
