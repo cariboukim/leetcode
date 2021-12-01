b7acaf9359aaa20592a8d871b48b578deb962ed4
Caleb Kim <kim.caleb10@gmail.com>
2021-11-30 16:28:41 -0800

    Initial solution, passing

---
+++ sqrt.js
@@ -0,0 +1,27 @@
+/*
+@param {number} x
+@return {number}
+*/
+
+// initial solution
+
+let test1 = 4;
+let test2 = 8;
+
+var mySqrt = (x) => {
+  let sqrt = 0;
+  let i = 1;
+
+  while (i < 100000) {
+    if (i**2 > x) {
+      sqrt = i - 1;
+      break;
+    }
+    i++;
+  }
+
+  return sqrt;
+}
+
+console.log(mySqrt(test1));
+console.log(mySqrt(test2));
