bf07638bfdfebaaa086ff07007c01774a32d3db6
Caleb Kim <kim.caleb10@gmail.com>
2021-12-01 23:51:51 -0800

    Started working on climbingStairs

---
+++ climbingStairs.js
@@ -0,0 +1,14 @@
+/*
+ * @param {number} n
+ * @return {number}
+ */
+
+let test1 = 2;
+let test2 = 3;
+
+var climbStairs = (n) => {
+
+}
+
+console.log(climbStairs(test1));
+console.log(climbStairs(test2));
